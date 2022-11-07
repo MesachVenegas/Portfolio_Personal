2d12718c9c7bb009d1e298207b8c92c4a9322e43
Mesach Venegas <mesach.venegas@hotmail.com>
2022-11-06 16:08:58 -0800

    add: mobile menu navigation

--- Js/main.js
+++ Js/main.js
@@ -3,9 +3,29 @@ function loading(){
     const loader = document.getElementById('loader_container');
     setTimeout( () => {
         loader.classList.add("hidden")
-    }, 3000)
+    }, 3000);
+}
+
+// Change lightmode to darkmode
+const darkMode = () =>{
+    const btnMode = document.getElementById('mode')
+    const body =  document.getElementById('body')
+    btnMode.addEventListener( 'click', e =>{
+        body.classList.toggle('dark')
+    });
+}
+
+// menu navigation mobile.
+const menu = () =>{
+    const btnMenu = document.getElementById('btn_menu')
+    const menu = document.getElementById('menu')
+    btnMenu.addEventListener('click', e =>{
+        menu.classList.toggle('nav__menu--open')
+    })
 }
 
 document.addEventListener( "DOMContentLoaded", () => {
     loading()
+    darkMode()
+    menu()
 });
