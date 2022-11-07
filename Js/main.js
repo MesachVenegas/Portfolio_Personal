168b93b71436d09cc7e4d1e15e88923a73b096a0
Mesach Venegas <mesach.venegas@hotmail.com>
2022-11-06 17:07:53 -0800

    add: menu btn animation

--- Js/main.js
+++ Js/main.js
@@ -18,9 +18,15 @@ const darkMode = () =>{
 // menu navigation mobile.
 const menu = () =>{
     const btnMenu = document.getElementById('btn_menu')
+    const stripe1 = document.getElementById('1')
+    const stripe2 = document.getElementById('2')
+    const stripe3 = document.getElementById('3')
     const menu = document.getElementById('menu')
     btnMenu.addEventListener('click', e =>{
         menu.classList.toggle('nav__menu--open')
+        stripe1.classList.toggle('stripe__open--1')
+        stripe2.classList.toggle('hidden')
+        stripe3.classList.toggle('stripe__open--2')
     })
 }
 
